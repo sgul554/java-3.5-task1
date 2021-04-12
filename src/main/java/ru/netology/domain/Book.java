530ebe54736f4b318181028fa949249ed74c5018
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-04-12 21:02:34 +0300

    Довавление кода и тестов

---
+++ src/main/java/ru/netology/domain/Book.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class Book extends Product {
+    private String author;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Book(String author) {
+        this.author = author;
+    }
+
+    public Book(int id, String name, int price, String author) {
+        super(id, name, price);
+        this.author = author;
+    }
+}
---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class Smartphone extends Product{
+    private String producer;
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public Smartphone(String producer) {
+        this.producer = producer;
+    }
+
+    public Smartphone(int id, String name, int price, String producer) {
+        super(id, name, price);
+        this.producer = producer;
+    }
+}
